118b8af8612f1913aedb9a5dc6472b201684a5c5
Dobby <66482172+1532298625@users.noreply.github.com>
2021-10-13 14:54:05 +0800

    first experiment

---
+++ CarTest/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace CarTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.恢复ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 4, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 612);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(778, 52);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_start_click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(158, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "暂停";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_stop_click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(313, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 46);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "加速";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_speedUp_click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(468, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(149, 46);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "减速";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_decelerate_click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(623, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(152, 46);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "结束程序";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_terminate_click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(3, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 600);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(160, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer_rePaint);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.恢复ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(117, 64);
+            // 
+            // 恢复ToolStripMenuItem
+            // 
+            this.恢复ToolStripMenuItem.Name = "恢复ToolStripMenuItem";
+            this.恢复ToolStripMenuItem.Size = new System.Drawing.Size(116, 30);
+            this.恢复ToolStripMenuItem.Text = "恢复";
+            this.恢复ToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem_restore);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(116, 30);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem_exit);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_mouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 664);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.form_formClosing);
+            this.Load += new System.EventHandler(this.form_load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.form_mouseClick);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 恢复ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ CarTest/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_rePaint(object sender, EventArgs e)
+        {
+            //控制车的移动
+            int move_x = panel1.Size.Width / 100;
+            int move_y = panel1.Size.Height / 100;
+            int car_x = pictureBox1.Location.X + move_x;
+            int car_y = pictureBox1.Location.Y + move_y;
+            //判断车是否超过窗体区域，超过则重新回到左上角
+            if (car_x > this.Width - pictureBox1.Size.Width || car_y > this.Height - pictureBox1.Size.Height)
+            {
+                car_x = 0;
+                car_y = 0;
+            }
+            //重新设置小车的位置
+            pictureBox1.Location = new Point(car_x, car_y);
+
+        }
+
+        private void button_start_click(object sender, EventArgs e)
+        {
+            //开启定时器，开始小车的移动
+            timer1.Start();
+        }
+
+        private void button_stop_click(object sender, EventArgs e)
+        {
+            //暂停定时器，暂停小车的移动
+            timer1.Stop();
+        }
+
+        private void button_speedUp_click(object sender, EventArgs e)
+        {   
+            //当刷新的时间间隔大于10ms
+            if(timer1.Interval > 20)
+            {
+                timer1.Interval -= 10;
+            }
+            //当刷新的时间间隔大于5ms小于10ms
+            else if(timer1.Interval > 5)
+            {
+                timer1.Interval -= 1;
+            }
+            else
+            {
+                timer1.Interval = 5;
+            }
+        }
+
+        private void button_decelerate_click(object sender, EventArgs e)
+        {
+            //当刷新的时间间隔小于10ms
+            if (timer1.Interval < 300)
+            {
+                timer1.Interval += 50;
+            }
+            else
+            {
+                timer1.Interval = 350;
+            }
+        }
+
+        private void button_terminate_click(object sender, EventArgs e)
+        {
+            //退出程序
+            System.Environment.Exit(0);
+        }
+
+        private void notifyIcon_mouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //显示本Winform程序
+            this.Show();
+        }
+
+        private void form_mouseClick(object sender, MouseEventArgs e)
+        {
+            //显示本WinForm程序
+            this.Show();
+        }
+
+        private void form_load(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(0, 0);//设置初始位置为最左上角
+        }
+
+        private void form_formClosing(object sender, FormClosingEventArgs e)
+        {
+            //设置点击“X”后不会退出程序
+            e.Cancel = true;
+            //隐藏本WinForm程序
+            this.Hide();
+        }
+
+        private void toolStripMenuItem_restore(object sender, EventArgs e)
+        {
+            //显示本WinForm程序
+            this.Show();
+        }
+
+        private void toolStripMenuItem_exit(object sender, EventArgs e)
+        {
+            //退出程序
+            System.Environment.Exit(0);
+        }
+    }
+}
